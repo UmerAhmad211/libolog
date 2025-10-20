af301b79b55ecd5b67b4e9ce25454308a4c0437c
UmerAhmad211 <umerahmed824@gmail.com>
2025-10-20 23:36:33 +0500

    Add olog_msg_verbose API

--- include/olog.h
+++ include/olog.h
@@ -1,12 +1,19 @@
 #ifndef OLOG_H
 #define OLOG_H
 
+#include <bits/pthreadtypes.h>
+#include <pthread.h>
+#include <stddef.h>
+#include <string.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <bits/pthreadtypes.h>
-#include <pthread.h>
+#define BUF_LEN_NV 256
+#define BUF_LEN_V 512
+#define FILENAME \
+	(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 enum Olog_Cntxt {
 	debug,
@@ -28,9 +35,12 @@ void
 olog_set_cntxt(const enum Olog_Cntxt);
 
 void
-olog(const char *, ...);
+olog(size_t, const char *, ...);
 
-#define olog_msg(fmt, ...) olog(fmt, ##__VA_ARGS__)
+#define olog_msg(fmt, ...) olog(BUF_LEN_NV, fmt __VA_OPT__(, ) __VA_ARGS__)
+#define olog_msg_verbose(fmt, ...)                                  \
+	olog(BUF_LEN_V, "[%s:%d:%s(...)] " fmt, FILENAME, __LINE__, \
+	     __func__ __VA_OPT__(, ) __VA_ARGS__)
 
 void
 olog_flush();
